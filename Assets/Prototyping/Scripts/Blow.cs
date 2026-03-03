8af7c600946ee20fa56244584015dd74ca38de4a
DiskKun <luca5h1905@gmail.com>
2026-03-02 19:20:06 -0500

    blowing prototype

---
+++ Assets/Prototyping/Scripts/Blow.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Blow : MonoBehaviour
+{
+    InputAction blow;
+    Rigidbody blownObjectRB;
+    public TextMeshProUGUI blowText;
+
+    public float blowForce;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        blow = InputSystem.actions.FindAction("Blow");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blow.WasPressedThisFrame())
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, 100f, LayerMask.GetMask("Blowable"));
+            if (hit.collider != null)
+            {
+                blownObjectRB = hit.collider.GetComponent<Rigidbody>();
+            }
+
+            blowText.text = "Blowing...";
+        }
+
+        if (blow.WasReleasedThisFrame())
+        {
+            blownObjectRB = null;
+            blowText.text = "Click to Blow";
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (blownObjectRB != null)
+        {
+            blownObjectRB.AddForce(transform.forward * Time.deltaTime * blowForce);
+        }
+    }
+}
