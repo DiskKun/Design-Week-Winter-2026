68507cfb3e623051b8a1ebb52bec14b030cd7155
HoffGang <hoffalex@sheridancollege.ca>
2026-03-04 13:11:40 -0500

    Leaf asset has been changed and reimported. Leaves are visible and blowable now.

--- Assets/Prototyping/Scripts/Blow.cs
+++ Assets/Prototyping/Scripts/Blow.cs
@@ -10,25 +10,51 @@ public class Blow : MonoBehaviour
     Rigidbody blownObjectRB;
     public TextMeshProUGUI blowText;
 
+    //Collider BlowCldr;
+    //RaycastHit BlowHit;
+
+    public float MaxBlowDist;
+    public float BlowSpeed;
+    private bool BlowDetect;
+
     public float blowForce;
+    public AudioSource Trumpet;
+    //private bool TrumpetSFXPlay = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         blow = InputSystem.actions.FindAction("Blow");
-
+        //BlowCldr = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       // TrumpetSFXPlay = false;
         if (blow.WasPressedThisFrame())
         {
+            //BlowDetect = Physics.BoxCast(BlowCldr.bounds.center, transform.localScale * 1, transform.forward, out BlowHit, transform.rotation, MaxBlowDist); 
+            //if (BlowDetect && BlowHit.collider != null)
+            //{
+            //    blownObjectRB = BlowHit.collider.GetComponent<Rigidbody>();
+            //}
             RaycastHit hit;
             Physics.Raycast(transform.position, transform.forward, out hit, 100f, LayerMask.GetMask("Blowable"));
             if (hit.collider != null)
             {
                 blownObjectRB = hit.collider.GetComponent<Rigidbody>();
             }
+
+            //else if (hit.collider != null && GameObject.FindWithTag("Trumpet"))
+            //{
+            //    TrumpetSFXPlay = true;
+            //    if (TrumpetSFXPlay == true)
+            //    {
+            //        Trumpet.Play();
+            //        Debug.Log("Trumpet sound is playing");
+            //    }
+            //}
 
             blowText.text = "Blowing...";
         }
