10b8bfbcdd6a6d176676745e241bb16bb935de91
DiskKun <luca5h1905@gmail.com>
2026-03-05 01:26:27 -0500

    add bubble wand

--- Assets/Prototyping/Scripts/Blow.cs
+++ Assets/Prototyping/Scripts/Blow.cs
@@ -9,12 +9,13 @@ public class Blow : MonoBehaviour
     Rigidbody blownObjectRB;
 
     private ParticleSystem particleSystem;
-    private bool blowingPlayer = false;
     private InputAction blowAction;
     
     public TextMeshProUGUI blowText;
     public CharacterController playerController;
     
+    public bool blowingPlayer = false;
+    public bool blowingBubble = false;
 
     public float blowObjectForce;
     public float minObjectForce;
@@ -50,17 +51,24 @@ public class Blow : MonoBehaviour
         if (blowAction.IsPressed()) // Check to see if the left mouse button is held down
         {
             // perform a raycast to see what's on the player's reticle, only include blowable and default layers
-            Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f, LayerMask.GetMask("Blowable", "Default"));
+            Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f, LayerMask.GetMask("Blowable", "Default", "Wand"));
             if (hit.transform != null) // if hit something...
             {
                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Blowable")) // and that thing is blowable...
                 {
                     blowingPlayer = false; // stop blowing the player
                     blownObjectRB = hit.collider.GetComponent<Rigidbody>(); // set the rigidbody of the hit object
+                    blowingBubble = false;
                 } else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Default")) // if that thing is a solid object...
                 {
                     blowingPlayer = true; // blow the player
                     blownObjectRB = null; // stop blowing the object
+                    blowingBubble = false;
+                } else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Wand"))
+                {
+                    blowingBubble = true;
+                    blowingPlayer = false;
+                    blownObjectRB = null;
                 }
             }
             
@@ -74,6 +82,7 @@ public class Blow : MonoBehaviour
             blowText.text = "Space to Blow";
             blowingPlayer = false; // stop blowing the player
             blownObjectRB = null; // stop blowing the object
+            blowingBubble = false;
         }
     }
 
---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log("Destroyed Bubble");
+        //Destroy(gameObject);
+    }
+}
